1347cc0e83ab27e651c947b6ee79747a78dd7b7c
Lau Delos <97628988+lauradlsantos@users.noreply.github.com>
2022-10-14 11:01:16 -0300

    Final details
    Finalized the last details of styles and colors.

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const marker = document.getElementById("marker");
 
+
 marker.addEventListener("click", function () {
+    
     console.log("hello");
-    document.getElementById("number").innerHTML = 0
+       document.getElementById("number").innerHTML = 0;
     let mark = document.getElementById("mark");
     mark.classList.remove("unread")
     let angela = document.getElementById("angela");
     angela.classList.remove("unread")
     let jacob = document.getElementById("jacob");
     jacob.classList.remove("unread")
-
+    let dotMark = document.getElementById("dotMark");
+    dotMark.classList.add("hiddenDot")
+    let dotAngela = document.getElementById("dotAngela");
+    dotAngela.classList.add("hiddenDot");
+    let dotJacob = document.getElementById("dotJacob");
+    dotJacob.classList.add("hiddenDot")
   });
 
